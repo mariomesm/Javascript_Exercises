764a913868546800a8d144bbe86a00a7dd082491
Mario Martinez <mariomartinezmesm@gmail.com>
2023-02-28 11:24:10 -0600

    feat: add setTimeOut in function

--- ejercicios_5/js/ejercicio_1.js
+++ ejercicios_5/js/ejercicio_1.js
@@ -1,14 +1,14 @@
-const water = () => console.log("El agua esta irviendo")
-const aboutJelly = () => console.log("El sobre esta vertido")
-const mix = () => console.log("La mezcla esta enfriándose")
+const water = () => console.log("Calentar Agua")
+const aboutJelly = () => console.log("Vertir el sobre ")
+const mix = () => console.log("Enfriar mezcla")
 const eat = () => console.log("Ya puedes comer tu gelatina!")
 
 const  prepareJelly = (cb1,cb2,cb3,cb4) => {
 
-    cb1()
-    cb2()
-    cb3()
-    cb4()
+    setTimeout(cb1,0)
+    setTimeout(cb2,5000)
+    setTimeout(cb3,10000)
+    setTimeout(cb4,15000)
 }
 
 let resultJelly = prepareJelly (water,aboutJelly,mix,eat)
