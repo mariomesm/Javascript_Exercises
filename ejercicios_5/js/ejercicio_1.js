6f4fdfac0c6ae27106deadbe31f64fb41c9a7349
Mario Martinez <mariomartinezmesm@gmail.com>
2023-02-27 21:27:51 -0600

    feat:exercise callback

---
+++ ejercicios_5/js/ejercicio_1.js
@@ -0,0 +1,14 @@
+const water = () => console.log("El agua esta irviendo")
+const aboutJelly = () => console.log("El sobre esta vertido")
+const mix = () => console.log("La mezcla esta enfriándose")
+const eat = () => console.log("Ya puedes comer tu gelatina!")
+
+const  prepareJelly = (cb1,cb2,cb3,cb4) => {
+
+    cb1()
+    cb2()
+    cb3()
+    cb4()
+}
+
+resultJelly = prepareJelly (water,aboutJelly,mix,eat)
